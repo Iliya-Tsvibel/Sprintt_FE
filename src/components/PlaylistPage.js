adc44124958ddfa253bbcd0a8da9168e969e848c
Iliya <zabaron@gmail.com>
2021-04-27 18:37:10 +0300

    playist table

--- src/components/PlaylistPage.js
+++ src/components/PlaylistPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 import PlaylistHeader from "./PlaylistHeader";
+import PlaylistTab from "./PlaylistTab";
+import "./PlaylistPage.css";
 
 const PlaylistPage = () => {
   return (
-    <div class="sticky">
-      <PlaylistHeader />
-    </div>
+    <>
+      <div class="sticky">
+        <PlaylistHeader />
+      </div>
+      <div className="playlisttab">
+        <PlaylistTab />
+      </div>
+    </>
   );
 };
 
